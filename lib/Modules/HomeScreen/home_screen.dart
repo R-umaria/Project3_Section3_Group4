2ea48154ab5d03967ca3a80c68eb17e2c03d9cbb
R-umaria <145788470+R-umaria@users.noreply.github.com>
2024-12-01 17:37:06 -0500

    buttons appear resonable

--- lib/Modules/HomeScreen/home_screen.dart
+++ lib/Modules/HomeScreen/home_screen.dart
@@ -68,10 +68,10 @@ class _HomeScreenState extends State<HomeScreen> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          backgroundColor: const Color.fromARGB(255, 202, 208, 214),
+          backgroundColor: const Color.fromRGBO(47, 47 , 47, 1),
           title: const Text(
             'Security Options',
-            style: TextStyle(color: Color.fromARGB(255, 28, 3, 34)),
+            style: TextStyle(color: Color.fromRGBO(247, 255, 247, 1)),
           ),
           content: Column(
             mainAxisSize: MainAxisSize.min,
@@ -121,17 +121,17 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(30.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ElevatedButton.icon(
               onPressed: _showSecurityOptions,
               style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromRGBO(41, 47, 54, 1),
+                backgroundColor: const Color.fromARGB(255, 47, 47, 47),
                 padding:
                     const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                textStyle: const TextStyle(fontSize: 16),
+                textStyle: const TextStyle(fontSize: 16, color: Color.fromRGBO(255, 255, 255, 1)),
               ),
               icon: const Icon(Icons.lock, color: Colors.white),
               label: const Text(
@@ -140,71 +140,98 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             const SizedBox(height: 16),
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                ),
-                itemCount: _rooms.length + 1,
-                itemBuilder: (context, index) {
-                  if (index == _rooms.length) {
-                    return GestureDetector(
-                      key: const Key('addRoomTile'),
-                      onTap: _addRoom,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: const Color.fromRGBO(78, 205, 196, 1),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: const Center(
-                          child: Icon(
-                            Icons.add,
-                            color: const Color.fromRGBO(247, 255, 247, 1),
-                            size: 36,
-                          ),
-                        ),
-                      ),
-                    );
-                  }
-                  final room = _rooms[index];
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => RoomsPage(room: room),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(8),
-                      
-                      decoration: BoxDecoration(
-                        color: const Color.fromRGBO(41, 47, 54, 1),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(Icons.meeting_room,
-                              color: Colors.white, size: 36),
-                          const SizedBox(height: 8),
-                          Text(
-                            room.name,
-                            style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
+
+
+          const SizedBox(height: 16), // Add spacing
+             // Rooms Title
+          const Padding(
+            padding: EdgeInsets.only(left: 8.0), // Add slight padding for alignment
+            child: Text(
+              'Rooms',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(41, 41, 41, 1),
               ),
             ),
+          ),
+          // Rooms Grid
+            Expanded(
+  child: Center(
+    child: SizedBox(
+      width: 1000,
+       // Set a max width for the grid
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // Keep 2 columns
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
+          childAspectRatio: 5, // Adjust as necessary
+        ),
+        itemCount: _rooms.length + 1,
+        itemBuilder: (context, index) {
+          if (index == _rooms.length) {
+            return GestureDetector(
+              key: const Key('addRoomTile'),
+              onTap: _addRoom,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(78, 205, 196, 1),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.add,
+                    color: Color.fromRGBO(247, 255, 247, 1),
+                    size: 28,
+                  ),
+                ),
+              ),
+            );
+          }
+          final room = _rooms[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => RoomsPage(room: room),
+                ),
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(6),
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(41, 47, 54, 1),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.meeting_room,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    room.name,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    ),
+  ),
+),
+
           ],
         ),
       ),
